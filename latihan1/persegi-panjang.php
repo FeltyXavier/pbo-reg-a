73fcaad58cae1e41b4277d58f61c0d2d27f20fbe
PutraYantu <putrayantu123@gmail.com>
2026-01-26 19:27:02 +0800

    Tambah latihan opp

--- latihan1/persegi-panjang.php
+++ latihan1/persegi-panjang.php
@@ -1,30 +1,35 @@
 <?php
-include 'fungsi.php';
+class PersegiPanjang{
+    public $panjang;
+    public $lebar;
 
-$id   = $_GET['id'];
-$data = getMahasiswaById($id);
+    public function hitungLuas(){
+        return $this->panjang; * $this->lebar;
+    }
 
-if (isset($_POST['update'])) {
-    updateMahasiswa($id, $_POST['nim'], $_POST['nama'], $_POST['prodi']);
-    header("Location: index.php");
+    public function hitungKeliling(){
+        return 2 * ($this->panjang + $this->lebar);
+    }
 }
+
+$pp1 = new PersegiPanjang();
+$pp1->panjang = 10;
+$pp1->lebar = 5;
+
+$pp2 = new PersegiPanjang();
+$pp2->panjang = 7;
+$pp2->lebar = 4;
+
+echo "<h3>Persegi Panjang 1</h3>";
+echo "Panjang : {$pp1->panjang}<br>";
+echo "Lebar : {$pp1->lebar}<br>";
+echo "Luas : {$pp1->hitungLuas()}<br>";
+echo "Keliling : {$pp1->hitungKeliling()}<br>";
+
+echo "<hr>";
+echo "<h3>Persegi Panjang 2</h3>";
+echo "Panjang : {$pp2->panjang}<br>";
+echo "Lebar : {$pp2->lebar}<br>";
+echo "Luas : {$pp2->hitungLuas()}<br>";
+echo "Keliling : {$pp2->hitungKeliling()}<br>";
 ?>
-<!doctype html>
-<html>
-<head>
-    <title>Edit Mahasiswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<div class="container mt-4">
-    <h3>Edit Mahasiswa</h3>
-    <form method="post">
-        <input name="nim" value="<?= $data['nim'] ?>" class="form-control mb-2">
-        <input name="nama" value="<?= $data['nama'] ?>" class="form-control mb-2">
-        <input name="prodi" value="<?= $data['prodi'] ?>" class="form-control mb-2">
-        <button name="update" class="btn btn-warning">Update</button>
-        <a href="index.php" class="btn btn-secondary">Kembali</a>
-    </form>
-</div>
-</body>
-</html>
